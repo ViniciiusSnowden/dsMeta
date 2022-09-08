c4052e360d134ff03d3fd8d261fa4701de85ad56
Paulo Vinicius Silva Costa <pauloviniciius@outlook.com>
2022-09-07 23:15:38 -0300

    [FRONTEND] useState

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -1,8 +1,16 @@
 import NotificationButton from "../NotificationButton";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
+import { useState } from "react";
 
 function SalesCard() {
+
+  const min = new Date().setDate((new Date().getDate() - 365));
+
+  const [minDate, setMinDate] = useState(new Date(min));
+  const [maxDate, setMaxDate] = useState(new Date());
+
+
   return (
     <div className="dsmeta-container">
       <div className="dsmeta-card">
@@ -10,16 +18,16 @@ function SalesCard() {
         <div>
           <div className="dsmeta-form-control-container">
             <DatePicker
-              selected={new Date()}
-              onChange={(date: Date) => {}}
+              selected={minDate}
+              onChange={(date: Date) => {setMinDate(date)}}
               className="dsmeta-form-control"
               dateFormat="dd/MM/yyyy"
             />
           </div>
           <div className="dsmeta-form-control-container">
             <DatePicker
-              selected={new Date()}
-              onChange={(date: Date) => {}}
+              selected={maxDate}
+              onChange={(date: Date) => {setMaxDate(date)}}
               className="dsmeta-form-control"
               dateFormat="dd/MM/yyyy"
             />
