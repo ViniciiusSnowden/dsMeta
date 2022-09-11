4939ff3fcd06b80b3086aba5a0b41687a4c1cd46
Paulo Vinicius Silva Costa <pauloviniciius@outlook.com>
2022-09-11 18:14:53 -0300

    [BACKEND] Twillo Sms

--- backend/src/main/java/com/pauloviniciius/dsmeta/SalesController.java
+++ backend/src/main/java/com/pauloviniciius/dsmeta/SalesController.java
@@ -2,13 +2,11 @@ package com.pauloviniciius.dsmeta;
 
 import com.pauloviniciius.dsmeta.entities.Sale;
 import com.pauloviniciius.dsmeta.services.SaleService;
+import com.pauloviniciius.dsmeta.services.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -17,6 +15,9 @@ public class SalesController {
     @Autowired
     private SaleService service;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(
             @RequestParam(value="minDate",defaultValue = "") String minDate,
@@ -24,5 +25,11 @@ public class SalesController {
             Pageable pageable) {
         return service.findSales(minDate, maxDate, pageable);
     }
+
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+        smsService.sendSms(id);
+    }
+
 
 }
